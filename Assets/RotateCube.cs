07c9a1e3c45d0180ef4e22948982683c7d023370
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-02-01 14:28:29 -0800

    initial commit

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
