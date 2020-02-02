b88d5fde0dc53f3536acd3fc265adce0a26294f6
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-02-01 18:28:24 -0800

    basic controls - click to move; predefine camera angles

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+
+    public LayerMask groundMask;
+    NavMeshAgent agent;
+    public GameObject spawnee;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            bool hit = Physics.Raycast(ray, out hitInfo, 1000, groundMask);
+            if (hit)
+            {
+                Debug.Log(hitInfo.point);
+                agent.SetDestination(hitInfo.point);
+                GameObject gameObject1 = Instantiate(spawnee, hitInfo.point, Quaternion.identity);
+            }
+        }
+        maybeUpdateCamera();
+        
+        Debug.Log(getCurrentRoom());
+    }
+
+    void maybeUpdateCamera()
+    {
+        GameObject currentRoomDefault = getCurrentRoom();
+        CameraView view = currentRoomDefault.GetComponent<CameraView>();
+        if (view != null)
+        {
+            Camera.main.transform.position = view.view.position;
+            Camera.main.transform.rotation = view.view.rotation;
+        }
+    }
+
+    // gets the "default" object of whatever room you're in
+    GameObject getCurrentRoom()
+    {
+        Ray ray = new Ray(transform.position, new Vector3(0, -1, 0));
+        RaycastHit hitInfo;
+        Physics.Raycast(ray, out hitInfo, 10, groundMask);
+        // Debug.Log(hitInfo.transform.gameObject.transform.parent);
+        return hitInfo.transform.gameObject;
+    }
+}
---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public float timeToFade = 0.2f;
+    float timeMade = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeMade = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentLeft = Mathf.Max(0, 1 - (Time.time - timeMade) / timeToFade);
+        transform.localScale = new Vector3(percentLeft, percentLeft, percentLeft);
+        if (percentLeft <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
